b5b164e419fbfbf59fe7c393c8a0f2d2af6a2e2b
cyypy <1540348104@qq.com>
2021-03-14 17:27:12 +0800

    three version

--- pu01.py
+++ pu01.py
@@ -1,4 +1,5 @@
 
 
 print("hello,pthon")
-print("xiao,cai,nihao")
+print("xiao,cai")
+print("how are you")
--- py02.py
+++ py02.py
@@ -1,2 +1,2 @@
-for i in range(5)
+for i in range(5):
     print("who are you {}".format(i))
